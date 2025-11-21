d8f4df3dc97e5282fb30173323c568adc5eed73d
Anderson Brasil <andersonbrasil72@gmail.com>
2025-11-21 13:45:41 -0300

    Push

---
+++ public/build/assets/edit-branch-C6YyAyDH.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-MW4Pb1eJ.js";function e(){return t.jsx("div",{children:"EditTenant"})}export{e as default};
---
+++ public/build/assets/edit-setting-BJZ2e8XQ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-MW4Pb1eJ.js";function i(){return t.jsx("div",{children:"EditSettingPage"})}export{i as default};
