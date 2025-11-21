81d2f01adfa7a70eeadc971e5bf9f2df840d3955
Anderson Brasil <andersonbrasil72@gmail.com>
2025-11-21 10:25:55 -0300

    Push

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        // host: 'https://techos.megb.com.br',
-        host: '172.16.1.67',
+        host: 'https://techos.megb.com.br',
+        // host: '172.16.1.67',
         // host: '192.168.2.53',
         cors: true
     },
