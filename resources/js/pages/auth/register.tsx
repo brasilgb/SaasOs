8a134c7b5490c882503063b4dffb56f279a995ee
Anderson Brasil <andersonbrasil72@gmail.com>
2025-12-04 15:48:30 -0300

    Push

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -47,7 +47,7 @@ export default function Register() {
         <AuthLayout title="Criar uma conta" description="Digite seus dados abaixo para criar sua conta">
             <Head title="Criar uma conta" />
             <div className='h-[70vh] overflow-y-auto'>
-                <form className="flex flex-col gap-6" onSubmit={submit}>
+                <form className="flex flex-col gap-6 mr-6" onSubmit={submit}>
                     <div className="grid gap-6">
 
                         <div className="grid gap-2">
@@ -168,8 +168,26 @@ export default function Register() {
                         </div>
 
                         <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
-                            {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                            Criar conta
+                            {processing ? (
+                                <>
+                                    <svg
+                                        className="animate-spin -ml-1 mr-2 h-4 w-4"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        ></path>
+                                    </svg>
+                                    Cadastrando...
+                                </>
+                            ) : (
+                                "Entrar"
+                            )}
                         </Button>
                     </div>
 
