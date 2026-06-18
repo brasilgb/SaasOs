919b845d456539acecf523bfe6666471396d024a
Anderson Brasil <andersonbrasil72@gmail.com>
2026-06-18 08:01:20 -0300

    Reapply "Push"
    This reverts commit 2612947630b397555670b563c8fc4a672d043688.

--- public/build/assets/edit-branch-CKZp7TrS.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-BFza9m97.js";function e(){return t.jsx("div",{children:"EditTenant"})}export{e as default};
--- public/build/assets/icon-bY2xV4lL.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./vendor-BFza9m97.js";import{c as t}from"./utils-DSiXWwrG.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
