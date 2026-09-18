189f4b1d90c8dc2cd695fa22d8c94aad0574a21d
Anderson Brasil <andersonbrasil72@gmail.com>
2026-09-18 13:58:25 -0300

    Push

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => env('DB_STRICT', false),
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 $mysqlAttrSslCa => env('MYSQL_ATTR_SSL_CA'),
